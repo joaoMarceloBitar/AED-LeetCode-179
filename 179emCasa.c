8f41ebba77e688a9c43edec026079ce8a4b6f934
joaoMarceloBitar <joaomarcelodu@gmail.com>
2026-02-15 23:14:49 -0300

    terminado

---
+++ 179emCasa.c
@@ -0,0 +1,70 @@
+// JOÃO MARCELO BITAR DUARTE
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char *largestNumber(int *nums, int numsSize);
+bool comparaMenorQue(char *num1s, char *num2s);
+
+int main()
+{
+    int numsSize = 5;
+    int nums[] = {3, 30, 34, 5, 9};
+
+    char *resultado = largestNumber(nums, numsSize);
+    printf("%s\n", resultado);
+
+    free(resultado);
+    return 0;
+}
+
+char *largestNumber(int *nums, int numsSize)
+{
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = 0; j < numsSize - 1; j++)
+        {
+            char s1[100], s2[100];
+            sprintf(s1, "%d", nums[j]);
+            sprintf(s2, "%d", nums[j + 1]);
+
+            if (comparaMenorQue(s1, s2))
+            {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+
+    char *res = (char *)malloc(1000 * sizeof(char));
+    res[0] = '\0';
+    for (int i = 0; i < numsSize; i++)
+    {
+        char temp[20];
+        sprintf(temp, "%d", nums[i]);
+        strcat(res, temp);
+    }
+    return res;
+}
+
+bool comparaMenorQue(char *num1s, char *num2s)
+{
+    char comb1[100], comb2[100];
+
+    strcpy(comb1, num1s);
+    strcat(comb1, num2s);
+    strcpy(comb2, num2s);
+    strcat(comb2, num1s);
+
+    if (strcmp(comb1, comb2) < 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+// pesquisei sobre o sprintf pq sabia que existia
+// e convertia int pra string mas não lembrava como usava!!
